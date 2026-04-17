162157d9a991164962e61efcf5361548c2efe63a
Matteo Zapparoli <zapparoli.matteo@gmail.com>
2026-04-17 11:02:48 +0200

    Update Mizar64.java

--- src/Java/org/mizar64/Mizar64.java
+++ src/Java/org/mizar64/Mizar64.java
@@ -12,9 +12,8 @@ package org.mizar64;
  * 
  */
 public class Mizar64 {
-    
-    public static final long MIZAR64_DEFAULT_SEED_C = 0L;
-    public static final long MIZAR64_DEFAULT_SEED_X = 0L;
+
+    public static final long MIZAR64_DEFAULT_SEED = 0L;
     
     /**
      * Internal counter state
@@ -26,22 +25,18 @@ public class Mizar64 {
      */
     private long state_x;
     
-    public Mizar64(long seed_c, long seed_x) {
-        this.state_c = seed_c;
-        this.state_x = seed_x;
-    }
-    
     public Mizar64(long seed) {
-        this(seed, ~seed);
+        this.state_c = 1
+        this.state_x = seed;
     }
     
     public Mizar64() {
-        this(MIZAR64_DEFAULT_SEED_C, MIZAR64_DEFAULT_SEED_X);
+        this(MIZAR64_DEFAULT_SEED);
     }
     
     public long nextLong() {
         // 1. Linear step: update counter
-        state_c = state_c * 0x5851f42d4c957f2dL + 0x14057b7ef767814fL;
+        state_c *= 0x9e3779b97f4a7c15L;
 
         // 2. Mixing step: cross-coupled XOR-rotation and multiplication
         return state_x = 0x9e3779b97f4a7c15L * ((state_c << 32) ^ (state_c >>> 32) ^
